c365c7dccdabaf936e6e26b3dccf3c14e1168ebc
GitHub Action <action@github.com>
2022-08-02 10:42:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/sumanit/plugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sumanit/plugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sumanit.plugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sumanit.plugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
